19eedb62a598627f4885812e5b995a3319d8800e
beranemo <beranemo@gmail.com>
2018-01-21 01:41:07 +0800

    set the monster element in constructor

--- L3/js/index.js
+++ L3/js/index.js
@@ -56,6 +56,15 @@ class Hero extends BaseCharacter {
 class Monster extends BaseCharacter {
   constructor(name, hp, ap) {
     super(name, hp, ap);
+    
+    this.element = document.getElementById("monster-image-block");
+    this.hpElement = document.getElementById("monster-hp");
+    this.maxHpElement = document.getElementById("monster-max-hp");
+    this.hurtElement = document.getElementById("monster-hp-hurt");
+    
+    this.hpElement.textContent = this.hp;
+    this.maxHpElement.textContent = this.maxHp;
+    
     console.log("遇到怪獸 " + this.name + "了！");
   }
   attack(character) {
